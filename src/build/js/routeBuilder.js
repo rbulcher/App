b3c9652d9531bc8ce1fa4abfe183c71391b0e47d
Ryan Bulcher <ryanbulcher@Ryans-MacBook-Pro.local>
2021-03-16 22:27:30 -0400

    update fetch

--- src/build/js/routeBuilder.js
+++ src/build/js/routeBuilder.js
@@ -2,7 +2,7 @@ const API_URL =
   window.location.hostname === "127.0.0.1" ||
   window.location.hostname === "localhost"
     ? "http://localhost:1337/api/logs"
-    : "https://routeplan.xyz/api/logs";
+    : "http://routeplan.xyz/api/logs";
 
     navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
         enableHighAccuracy: true,
@@ -55,12 +55,12 @@ const displayRoutes = document.getElementById("routes");
       }
 
       
-      async function displayCurrentDay(day, dayAbreviation) {
+      function displayCurrentDay(day, dayAbreviation) {
         displayRoutes.textContent = '';
         const header = document.createElement("h2");
         header.textContent = day + " Route";
         displayRoutes.appendChild(header);
-       await fetch(API_URL)
+       fetch(API_URL)
     .then(response => response.json())
     .then(locations => {
           locations.forEach(location => {

24d0027eae4b76d74ac664ee36060a72b8d7d59a
Ryan Bulcher <ryanbulcher@Ryans-MacBook-Pro.local>
2021-03-18 22:22:06 -0400

    Api example findMonday

--- src/server.js
+++ src/server.js
@@ -1,38 +1,36 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 1337;
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const logs = require('./api/logs');
+const middlewares = require("./middlewares");
+const logs = require("./api/logs");
 
 const app = express();
-const path = require('path')
-app.use(morgan('common'));
+const path = require("path");
+app.use(morgan("common"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("express"));
 
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+app.use("/api/logs", logs);
+app.use("/api/logs/findLocation", logs);
 
-app.use('/api/logs', logs);
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-
 app.listen(port);
-

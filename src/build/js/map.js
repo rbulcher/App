64cd4851826b8197144c0e7db2bc0b665616158e
Ryan Bulcher <rjbulcher@gmail.com>
2021-03-23 14:04:31 -0400

    Onclick function update

--- src/build/js/map.js
+++ src/build/js/map.js
@@ -134,6 +134,23 @@ map.on("load", function () {
     },
   });
 
+  map.on("click", function (e) {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ["deliveriesLayer"], // replace this with the name of the layer
+    });
+
+    if (!features.length) {
+      return;
+    }
+
+    var feature = features[0];
+    var popup = new mapboxgl.Popup({ offset: [0, -15] })
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML("<h3>" + feature.properties.address + "</h3>")
+      .setLngLat(feature.geometry.coordinates)
+      .addTo(map);
+  });
+
   map.addLayer(
     {
       id: "routeLayer",
@@ -342,6 +359,7 @@ const setStops = function (stops) {
   stops.forEach((stop) => {
     const delivery = {
       properties: {
+        address: stop.address,
         name: stop.name,
         stop_number: stop.waypoint_index,
       },

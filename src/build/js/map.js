8de0053333af2267eb7aa0081494a3eab7785791
Ryan Bulcher <rjbulcher@gmail.com>
2021-04-18 23:03:26 -0400

    Test userlocation TTS directions

--- src/build/js/map.js
+++ src/build/js/map.js
@@ -241,10 +241,9 @@ function getStartLocation() {
   });
 }
 
-
-
 function routeBuild(day, currentRoute) {
-  if(currentRoute == 0) {
+  setInterval(updateUserLocation, 1000);
+  if (currentRoute == 0) {
     getStartLocation();
   }
   instructions.style.display = "";
@@ -263,7 +262,6 @@ function routeBuild(day, currentRoute) {
   locations
     .then((data) => (addresses = data))
     .then(() => {
-      
       const coords = [];
 
       var sortedAddresses = sortAddresses(addresses);
@@ -304,7 +302,6 @@ function routeBuild(day, currentRoute) {
           "&geometries=geojson&overview=full&steps=true&source=first&destination=last&roundtrip=false";
         optimizeUrl += "&approaches=" + approachParam.repeat(coords.length - 1);
 
-        
         fetch(optimizeUrl)
           .then((res) => res.json())
           .then((res) => {
@@ -341,15 +338,12 @@ function routeBuild(day, currentRoute) {
 const titleText = document.getElementById("title");
 const addressList = document.getElementById("addresses");
 
-
 const setOverview = async function (route, day) {
-
   const API_URL_DAY = "https://www.routeplan.xyz/api/logs/find" + day;
   const locations = getLocations(API_URL_DAY);
 
   var addresses = [];
-  await locations
-    .then((data) => (addresses = data))
+  await locations.then((data) => (addresses = data));
 
   const trip = route.trips[0];
   const waypoints = route.waypoints;
@@ -362,7 +356,7 @@ const setOverview = async function (route, day) {
   // Add the delivery addresses and turn-by-turn instructions to the sidebar for each leg of the trip
   trip.legs.forEach((leg, i) => {
     const listItem = document.createElement("li");
-    
+
     // We want the destination address when we depart, hence index + 1
     if (i < trip.legs.length) {
       const nextDelivery = waypoints.find(
@@ -374,33 +368,64 @@ const setOverview = async function (route, day) {
         let dateAndType = "";
         for (let i = 0; i < addresses.length; i++) {
           const address = addresses[i];
-          if(address.address == nextDelivery.address){
+          if (address.address == nextDelivery.address) {
             dateAndType = address.deliverDateAndType;
             let dayAbreviation = "";
-            if(day === "Monday"){ dayAbreviation = "Mon" }
-            if(day === "Tuesday") {dayAbreviation = "Tue"}
-            if(day === "Wednesday"){ dayAbreviation = "Wed"}
-            if(day === "Thursday") {dayAbreviation = "Thur"}
-            if(day === "Friday") {dayAbreviation = "Fri"}
-            if(day === "Saturday"){ dayAbreviation = "Sat"}
-            if(day === "Sunday"){ dayAbreviation = "Sun"}
+            if (day === "Monday") {
+              dayAbreviation = "Mon";
+            }
+            if (day === "Tuesday") {
+              dayAbreviation = "Tue";
+            }
+            if (day === "Wednesday") {
+              dayAbreviation = "Wed";
+            }
+            if (day === "Thursday") {
+              dayAbreviation = "Thur";
+            }
+            if (day === "Friday") {
+              dayAbreviation = "Fri";
+            }
+            if (day === "Saturday") {
+              dayAbreviation = "Sat";
+            }
+            if (day === "Sunday") {
+              dayAbreviation = "Sun";
+            }
 
             let start = dateAndType.search(dayAbreviation);
-            let splitFromStart = dateAndType.substring(start,dateAndType.length - 1);
+            let splitFromStart = dateAndType.substring(
+              start,
+              dateAndType.length - 1
+            );
             let findFinish = "";
 
-            if(day === "Monday"){ findFinish = "Tue" }
-            if(day === "Tuesday") {findFinish = "Wed"}
-            if(day === "Wednesday"){ findFinish = "Thur"}
-            if(day === "Thursday") {findFinish = "Fri"}
-            if(day === "Friday") {findFinish = "Sat"}
-            if(day === "Saturday"){ findFinish = "Sun"}
+            if (day === "Monday") {
+              findFinish = "Tue";
+            }
+            if (day === "Tuesday") {
+              findFinish = "Wed";
+            }
+            if (day === "Wednesday") {
+              findFinish = "Thur";
+            }
+            if (day === "Thursday") {
+              findFinish = "Fri";
+            }
+            if (day === "Friday") {
+              findFinish = "Sat";
+            }
+            if (day === "Saturday") {
+              findFinish = "Sun";
+            }
 
             let finish = splitFromStart.search(findFinish);
-            if(finish == "") {finish = splitFromStart.length -1}
+            if (finish == "") {
+              finish = splitFromStart.length - 1;
+            }
 
-            dateAndType = splitFromStart.substring(0,finish);
-            newsPaperType = dateAndType.substring(4,dateAndType.length);
+            dateAndType = splitFromStart.substring(0, finish);
+            newsPaperType = dateAndType.substring(4, dateAndType.length);
           }
         }
         listItem.innerHTML = `<b>Deliver to: ${nextDelivery.address} : Paper Type - [${newsPaperType}]</b>`;
@@ -409,7 +434,41 @@ const setOverview = async function (route, day) {
 
     addressList.appendChild(listItem);
     // add the TBT instructions for this leg
+    setInterval(() => {
+      leg.steps.forEach((step) => {
+        stepX = step.geometry.coordinates[0][0];
+        stepY = step.geometry.coordinates[0][1];
+        userX = userLocation[0];
+        userY = userLocation[1];
+        if (userX <= stepX + 0.0008 && userX >= stepX - 0.0008) {
+          if (userY <= stepY + 0.0008 && userY >= stepY - 0.0008) {
+            if (!alreadyVisitedStops.includes(step.name)) {
+              alreadyVisitedStops.push(step.name);
+              var msg = new SpeechSynthesisUtterance();
+              msg.text = step.maneuver.instruction;
+              window.speechSynthesis.speak(msg);
+            }
+          }
+        }
+      });
+    }, 1000);
+    let alreadyVisitedStops = [];
+
     leg.steps.forEach((step) => {
+      // stepX = step.geometry.coordinates[0][0];
+      // stepY = step.geometry.coordinates[0][1];
+      // userX = userLocation[0];
+      // userY = userLocation[1];
+      // if (userX <= stepX + 0.0008 && userX >= stepX - 0.0008) {
+      //   if (userY <= stepY + 0.0008 && userY >= stepY - 0.0008) {
+      //     if (!alreadyVisitedStops.includes(step.name)) {
+      //       alreadyVisitedStops.push(step.name);
+      //       var msg = new SpeechSynthesisUtterance();
+      //       msg.text = step.maneuver.instruction;
+      //       window.speechSynthesis.speak(msg);
+      //     }
+      //   }
+      // }
       const listItem = document.createElement("li");
       listItem.innerText = step.maneuver.instruction;
       addressList.appendChild(listItem);
